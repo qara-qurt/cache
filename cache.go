bdc0acb3e9b38fc332f95d76a5c228b04d4dbe0b
Dias <serikov.2002.12@gmail.com>
2023-02-17 23:52:00 +0600

    modify

--- cache.go
+++ cache.go
@@ -15,11 +15,21 @@ func New() *Cache {
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key string, value interface{}) error {
+	if key := validate(key); key != nil {
+		return errors.New("key is empty")
+	}
+
 	c.data[key] = value
+
+	return nil
 }
 
 func (c *Cache) Get(key string) (interface{}, error) {
+	if key := validate(key); key != nil {
+		return nil, errors.New("key is empty")
+	}
+
 	val, ok := c.data[key]
 	if !ok {
 		return nil, errors.New(fmt.Sprintf("Value with %s doesn't exist", key))
@@ -28,9 +38,21 @@ func (c *Cache) Get(key string) (interface{}, error) {
 }
 
 func (c *Cache) Delete(key string) error {
+	if key := validate(key); key != nil {
+		return errors.New("key is empty")
+	}
+
 	if _, ok := c.data[key]; !ok {
 		return errors.New(fmt.Sprintf("Value with %s doesn't exist", key))
 	}
 	delete(c.data, key)
+	return nil
+}
+
+func validate(val string) error {
+	if val == "" {
+		return errors.New("key is empty")
+	}
+
 	return nil
 }
